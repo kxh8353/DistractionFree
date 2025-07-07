7607ecd72d73bc87eb2cc63065c656524b0f528d
kxh8353 <kxh8353@rit.edu>
2025-07-07 01:30:10 -0400

    differentiate event handling for operating system type

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void OperatingSystem(String args){
+        System.out.println("What Operating System do you use?");
+        if (args == "Windows" || args == "windows"){
+
+        } else if (args == "Mac" || args == "Macbook"){
+
+        }
+    }
+    
+    public static void SelectWebsites(String[] args){
+        System.out.println("Select Website(s) to Block:");
+    }
+
+    public static void main(String[] args){
+        
     }
 }
