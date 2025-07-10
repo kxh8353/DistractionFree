2e1c79e8e1f63f8ddba250121a0148568c12e79c
kxh8353 <kxh8353@rit.edu>
2025-07-09 23:01:03 -0400

    switched detecting applications from chhrome to detecting applications in safari

--- src/FileDetectors/DetectorForMacs.java
+++ src/FileDetectors/DetectorForMacs.java
@@ -10,26 +10,20 @@ public class DetectorForMacs {
         try {
             String[] cmd = {
                 "osascript",
-                "-e",
-                "tell application \"Google Chrome\"",
-                "-e",
-                "set window_list to every window",
-                "-e",
-                "set tab_list to {}",
-                "-e",
-                "repeat with the_window in window_list",
-                "-e",
-                "repeat with the_tab in every tab of the_window",
-                "-e",
-                "set end of tab_list to title of the_tab & \" - \" & URL of the_tab",
-                "-e",
-                "end repeat",
-                "-e",
-                "end repeat",
-                "-e",
-                "return tab_list",
-                "-e",
-                "end tell"
+                "-e", "tell application \"Safari\"",
+                "-e", "set window_list to every window",
+                "-e", "set tab_list to {}",
+                "-e", "repeat with the_window in window_list",
+                "-e", "repeat with the_tab in every tab of the_window",
+                "-e", "set the_url to URL of the_tab",
+                "-e", "set the_title to title of the_tab",
+                "-e", "if the_url contains \"google.com\" then",
+                "-e", "set end of tab_list to the_title",
+                "-e", "end if",
+                "-e", "end repeat",
+                "-e", "end repeat",
+                "-e", "return tab_list",
+                "-e", "end tell"
             };
 
             Process process = Runtime.getRuntime().exec(cmd);
