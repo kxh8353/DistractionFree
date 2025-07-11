60a71a10ac5d9c57a8f081c0e4ba30be679d08fc
kxh8353 <kxh8353@rit.edu>
2025-07-11 00:44:50 -0400

    added chrome app detection for macs

--- src/FileDetectors/DetectorForMacs.java
+++ src/FileDetectors/DetectorForMacs.java
@@ -8,7 +8,7 @@ public class DetectorForMacs {
     
     public static void DetectFilesOnMacs(){
         try {
-            String[] cmd = {
+            String[] SafariCMD = {
                 "osascript",
                 "-e", "tell application \"Safari\"",
                 "-e", "set tab_list to {}",
@@ -25,10 +25,45 @@ public class DetectorForMacs {
                 "-e", "end tell"
             };
 
-            Process process = Runtime.getRuntime().exec(cmd);
+            Process process = Runtime.getRuntime().exec(SafariCMD);
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
-            System.out.println("Open Tabs:");
+            System.out.println("Open Tabs on Safari:");
+
+            while ((line = reader.readLine()) != null){
+                String[] websites = line.split(",\\s*");
+                for (String website: websites){
+                    System.out.println("- " + website.trim());
+                }
+            }
+            process.waitFor();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            String[] ChromeCMD = {
+                "osascript",
+                "-e", "tell application \"Google Chrome\"",
+                "-e", "set tab_list to {}",
+                "-e", "repeat with w in windows",
+                "-e", "repeat with t in tabs of w",
+                "-e", "set the_url to URL of t",
+                "-e", "set the_title to name of t",
+                "-e", "if the_url contains \".com\" then",
+                "-e", "copy the_title to end of tab_list",
+                "-e", "end if",
+                "-e", "end repeat",
+                "-e", "end repeat",
+                "-e", "return tab_list",
+                "-e", "end tell"
+            };
+
+            Process process = Runtime.getRuntime().exec(ChromeCMD);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            System.out.println("Open Tabs on Chrome:");
 
             while ((line = reader.readLine()) != null){
                 String[] websites = line.split(",\\s*");
